921ac816354ed1e4c1a5ef85cf611abe874b435e
Maizatul-Mior <maizatulmaisarahmior@gmail.com>
2026-05-21 17:16:27 +0800

    Add Ebright logo from Google Drive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
 import TrialForm from './TrialForm'
 
-const LOGO_URL = 'PLACEHOLDER'
+const LOGO_URL = 'https://drive.google.com/uc?export=view&id=1Lqhqp2G9ycW1WrDT7iB3HXHc7WYS3eSb'
 
 export default function App() {
   return (
     <div className="page">
       <div className="page-header">
-        {LOGO_URL !== 'PLACEHOLDER' && (
-          <img src={LOGO_URL} alt="Ebright logo" className="logo" />
-        )}
+        <img src={LOGO_URL} alt="Ebright logo" className="logo" />
       </div>
       <div className="form-wrapper">
         <TrialForm />
